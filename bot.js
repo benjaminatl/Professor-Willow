d2e929b083d7a7d4416acdc2f76d3ed4c43413a4
MrRecordHolder <33103968+benjaminatl@users.noreply.github.com>
2018-11-09 11:24:31 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,15 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json");
 
-client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+client.on('ready', () => {
+    console.log('I am ready!');
 });
-  
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
